ae47d310a9ef0760890a5d91c33ad198ad77077a
prochka15nik06@gmail.com <prochka15nik06@gmail.com>
2024-05-11 16:17:59 +0600

    reg 2

--- recipes/views.py
+++ recipes/views.py
@@ -17,7 +17,7 @@ def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
+            username = form.cleaned_data['useremail']
             password = form.cleaned_data['password']
             user = authenticate(request, username=username, password=password)
             if user is not None:
