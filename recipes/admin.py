65c4170bcd045a9633c38da243f5ca622c68613e
prochka15nik06@gmail.com <prochka15nik06@gmail.com>
2024-06-06 20:06:06 +0600

    profile img

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from .models import Profile, Categories, Add_a_recipe_Model, CommentModel
+from .models import Profile, Categories, Add_a_recipe_Model, CommentModel, Detail
 
 admin.site.register(Profile)
 admin.site.register(Categories)
 admin.site.register(Add_a_recipe_Model)
 admin.site.register(CommentModel)
+admin.site.register(Detail)
+
 
 
